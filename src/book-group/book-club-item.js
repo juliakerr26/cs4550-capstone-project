fd3b975c6dc21a95428f97f78bd53115d2dd9230
Julia Kerr <juliakerr26@gmail.com>
2023-04-19 10:30:05 -0400

    Format book club item image

--- src/book-group/book-club-item.js
+++ src/book-group/book-club-item.js
@@ -25,7 +25,7 @@ const BookClubItem = (
     <li className="list-group-item bg-light-green m-1 p-2">
       <div className="row">
         <div className="col-10">
-          <img width={70} className="float-start rounded-circle" src={`/images/book-stack.jpg`} />
+          <img width={70} className="float-start rounded me-2" src={`/images/book-stack.jpg`} />
           <div>
             <LinkContainer to={`/book-clubs/${club._id}`}>
               <div className="fw-bolder txt-dark-orange">{club.name}</div>
